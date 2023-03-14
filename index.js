5c9303e0c51efeee8192274c0022ea1f3021c241
Marcelo Machado <mmachado@ibm.com>
2023-03-14 02:39:26 -0300

    Add dependencie files

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023, IBM Research
+ * Licensed under The MIT License [see LICENSE for details]
+ */
+
+module.exports = {
+  datasources: {
+    ConceptNetDatasource: require('./lib/datasource/ConceptNetDatasource'),
+  },
+};
